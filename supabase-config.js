15bb33ab8e8d2c9957e2219e8ba4baff764f7a70
Pxcheco <pachecofabian764@gmail.com>
2026-04-19 17:57:09 -0400

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.OLON_SUPABASE = {
+  url: "https://howjgbpmllzlbmslxvqz.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvd2pnYnBtbGx6bGJtc2x4dnF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MjE1NzksImV4cCI6MjA5MjE5NzU3OX0.V39TqJDB2rUIFREX-Utxpn4wrN69UyEYgLZ4MmdSRt8"
+};
